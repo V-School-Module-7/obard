a1cfb10f638761dddfac7f27bf87bf4527a693e0
Jeremy Bleak <Jeremybleak99@gmail.com>
2020-12-04 18:43:27 -0700

    landing page in the works

---
+++ client/src/components/user/Landing.js
@@ -0,0 +1,16 @@
+const { renderSync } = require("node-sass");
+
+import React, { Component } from 'react'
+
+export default class Landing extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        )
+    }
+}
