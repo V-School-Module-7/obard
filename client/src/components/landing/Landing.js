33640eb7a98f8adeea2e81586ef341460fe4f413
Jeremy Bleak <Jeremybleak99@gmail.com>
2021-02-02 18:11:39 -0700

    log in and sign up components switched to class

--- client/src/components/landing/Landing.js
+++ client/src/components/landing/Landing.js
@@ -1,19 +1,26 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Login from './Login'
+import SignUp from './SignUp'
 
-export default function Landing(){
-    return(
-        <div>
+export default class Landing extends Component {
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             account: true
+        }
+    }
+    
+    render() {
+        return(
             <div>
-                <div></div>
                 <div>
-                    <form action="">
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                        <input type="text"/>
-                    </form>
+                    <div></div>
+                    <div>
+                        {this.state.account = true ? Login : SignUp}
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
---
+++ client/src/components/landing/Login.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default function Login() {
+        return (
+            <div>
+                <form action=""></form>
+            </div>
+        )
+}
